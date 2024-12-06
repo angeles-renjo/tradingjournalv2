8242f0c449ebfb752b6ef80ddd8d79d39879efb9
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2024-12-06 23:41:47 +0800

    change title and desciption of the webapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Trading Journal",
+  description: "Start being a discipline trader and achieve your goals",
 };
 export default function RootLayout({
   children,
